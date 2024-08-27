5a4f24f31c972766d503e1eacea81739d06e98b3
Maxclayton <maxclayton07@gmail.com>
2024-08-27 15:14:33 -0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react';
+import FormItems from './components/Form';
+import './App.css';
+import { motion } from "framer-motion";
+
+function App() {
+  const changingTexts = ['cool', 'great', 'the best'];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex(prevIndex => (prevIndex + 1) % changingTexts.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [changingTexts.length]);
+
+  return (
+    <>
+      <div className='app-container'>
+        <motion.div
+          initial={{ y: -200, scale: 0 }}
+          animate={{ y: 0, scale: 1 }}>
+          <img
+            src='https://media.licdn.com/dms/image/v2/D560BAQH1VMt-sVyrpw/company-logo_200_200/company-logo_200_200/0/1715283797157/motivosity_logo?e=1732752000&v=beta&t=6Mgnbjc1_CSBKXGMQ-VJJk8n_In9rWX7PRDrH3LN0gU'
+            className='logo-img'
+          />
+        </motion.div>
+        <motion.div
+          initial={{ y: -50, scale: 0 }}
+          animate={{ y: 0, scale: 1 }}
+          transition={{ delay: .15 }}
+        >
+          <img
+            src='https://www.motivosity.com/static/cae2ad014aaa8ad77fa8649fcc49cf2f/f5a82/hero.webp'
+            className='hero-img' />
+        </motion.div>
+        <motion.div
+          initial={{ y: -50, scale: 0 }}
+          animate={{ y: 0, scale: 1 }}
+          transition={{ delay: .25 }}
+        >
+          <div className='headline'>
+            <h1>Motivosity is</h1>
+            <motion.div
+              key={currentIndex}
+              className='changing-text'
+              initial={{ y: 20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: 50 }}
+              transition={{ duration: 0.25 }}
+            >
+              {changingTexts[currentIndex]}
+            </motion.div>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ y: -50, scale: 0 }}
+          animate={{ y: 0, scale: 1 }}
+          transition={{ delay: .35 }}>
+          <FormItems />
+        </motion.div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,67 @@
+import './Form.css';
+import { useState } from 'react';
+
+function FormItems() {
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const formItems = [
+    { label: 'Name:', type: 'text', id: 'name', placeHolder: 'John Doe' },
+    { label: 'Email:', type: 'email', id: 'email', placeHolder: 'trevorlawrence@jags.com' },
+    { label: 'Phone:', type: 'tel', id: 'phone', placeHolder: '1234568970' }
+  ];
+
+  const validateForm = (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+
+    if (!name) {
+      setErrorMessage('Please enter your name.');
+      return;
+    }
+
+    if (!email || !/\S+@\S+\.\S+/.test(email)) {
+      setErrorMessage('Please enter a valid email.');
+      return;
+    }
+
+    if (!phone || !/^\d{10}$/.test(phone)) {
+      setErrorMessage('Please enter a valid 10-digit phone number.');
+      return;
+    }
+
+    setErrorMessage('');
+    setIsSubmitted(true);
+  };
+
+  return (
+    <>
+      <div className={`form-container ${isSubmitted ? 'hidden' : ''}`}>
+        <form className='form' onSubmit={validateForm} noValidate>
+          {formItems.map(({ label, type, id, placeHolder }) => (
+            <div key={id} className='form-item'>
+              <label className='form-label'>{label}</label>
+              <input type={type} id={id} name={id} className='form-input' placeholder={placeHolder} />
+            </div>
+          ))}
+          <div className='button-container'>
+            <button type="submit" className='form-button'>Submit</button>
+          </div>
+          {errorMessage && (
+            <div className='error-message'>
+              {errorMessage}
+            </div>
+          )}
+        </form>
+      </div>
+      <div className={`thank-you-container ${isSubmitted ? '' : 'hidden'}`}>
+        <span className='thanks-text'>Thank you! We will reach out shortly.</span>
+        <a href=''>Submit again?</a>
+      </div>
+    </>
+  );
+}
+
+export default FormItems;
